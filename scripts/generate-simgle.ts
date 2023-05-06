fe7f75cf02050013eb995fe0ed58268cf37f9c28
taiyuuki <taiyuuki@qq.com>
2023-05-06 13:56:46 +0800

    feat: 生成单文件的脚本

---
+++ scripts/generate-simgle.ts
@@ -0,0 +1,48 @@
+import path from 'path'
+import fs from 'fs-extra'
+import prompts from 'prompts'
+import c from 'picocolors'
+import { loadQuizByNo, resolveInfo } from './loader'
+import { formatToCode } from './actions/utils/formatToCode'
+import { getQuestionFullName } from './actions/issue-pr'
+import type { QuizMetaInfo } from './types'
+
+// 替换部分中文题目的难度标识为英文
+const REPLACE: Record<string, string> = {
+  简单: 'easy',
+  中等: 'medium',
+  困难: 'hard',
+}
+const LANGRUAGE = 'zh-CN'
+
+async function generateSimgle() {
+  console.log(' ')
+  const result = await prompts([{
+    type: 'number',
+    name: 'num',
+    message: '请输入题目序号：',
+  }])
+  if (!result?.num)
+    return console.log(c.yellow('已取消'))
+
+  const num = String(result.num).padStart(5, '0')
+  const quiz = await loadQuizByNo(num)
+  if (!quiz) {
+    console.log(' ')
+    return console.log(c.yellow(`不存在的题目序号：${num}`))
+  }
+  let { difficulty, title } = resolveInfo(quiz, LANGRUAGE) as QuizMetaInfo & { difficulty: string }
+  if (difficulty in REPLACE)
+    difficulty = REPLACE[difficulty]
+  const code = formatToCode(quiz, LANGRUAGE)
+  const quizePath = path.join(__dirname, '../answers', difficulty)
+  const filepath = path.join(quizePath, `${getQuestionFullName(quiz.no, difficulty, title)}.ts`)
+  if (fs.existsSync(filepath))
+    return console.log(`${c.bold(c.red('文件已存在:'))} ${c.dim(filepath)}`)
+  await fs.ensureDir(quizePath)
+  await fs.writeFile(filepath, code, 'utf-8')
+  console.log(' ')
+  console.log(`${c.bold(c.green('已生成:'))} ${c.dim(filepath)}`)
+}
+
+generateSimgle()
